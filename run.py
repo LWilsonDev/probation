833440fbd2107467f572989677513f1712758679
Lucy Wilson <lucyleleu@googlemail.com>
2018-05-30 16:22:37 +0000

    logic corrected for date display

--- run.py
+++ run.py
@@ -5,8 +5,7 @@ from datetime import timedelta
 
 app = Flask(__name__)
         
-stored_days = get_stored_days()
-end_date = get_end_date()
+
 
 def write_to_file(filename, data):
     with open(filename, 'a') as file:
@@ -19,14 +18,15 @@ def get_all_comments():
     return comments
     
 def add_comments(username, comment, days):
-    write_to_file('data/comments.txt', "({0}) {1} Days = {2} Reason: {3}\n".format(
-            datetime.now().strftime("%H:%M"),
+    write_to_file('data/comments.txt', "({0}) {1} added/subtracted {2} days because: {3}\n".format(
+            datetime.now().strftime("%A %d %B"),
             username,
             days,
             comment))
     return redirect(request.form) 
     
 def add_days(days):
+    stored_days = get_stored_days()
     with open('data/days.txt', 'w+') as file:
         total_days = int(stored_days) + int(days)
         file.writelines (str(total_days)) 
@@ -37,7 +37,9 @@ def get_stored_days():
         stored_days = current_days.read()
         return stored_days
         
-def get_end_date():
+def get_end_date(stored_days):
+    
+    stored_days = get_stored_days()
     date = datetime.now() + timedelta(int(stored_days))
     end_date = date.strftime('%A %d %B %Y')
     return end_date
@@ -48,7 +50,8 @@ def get_end_date():
         
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
+    stored_days = get_stored_days()
+    end_date = get_end_date(stored_days)
     if request.method == 'POST':
         write_to_file('data/users.txt', request.form['username'] + '\n')
         return redirect(request.form['username'])
@@ -58,13 +61,14 @@ def index():
 @app.route('/<username>', methods=['GET', 'POST'])
 def user(username):
     comments = get_all_comments()
-    end_date = get_end_date()
+    stored_days = get_stored_days()
+    end_date = get_end_date(stored_days)
     if request.method == 'POST':
         comment = request.form['comment']
         days = request.form['days']
         add_comments(username, comment, days)
         add_days(days)
-        end_date = get_end_date()
+        end_date = get_end_date(stored_days)
        
         return redirect(username)
     return render_template("log.html",
