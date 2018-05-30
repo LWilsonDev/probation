8d83e7ed84368aff79b2aad8e8a284b4d1db9eec
Lucy Wilson <lucyleleu@googlemail.com>
2018-05-30 15:20:26 +0000

    all files added

--- run.py
+++ run.py
@@ -4,7 +4,9 @@ from datetime import datetime
 from datetime import timedelta  
 
 app = Flask(__name__)
-
+        
+stored_days = get_stored_days()
+end_date = get_end_date()
 
 def write_to_file(filename, data):
     with open(filename, 'a') as file:
@@ -23,47 +25,50 @@ def add_comments(username, comment, days):
             days,
             comment))
     return redirect(request.form) 
+    
+def add_days(days):
+    with open('data/days.txt', 'w+') as file:
+        total_days = int(stored_days) + int(days)
+        file.writelines (str(total_days)) 
+    return redirect(request.form)    
 
 def get_stored_days():
     with open('data/days.txt', 'r') as current_days:
         stored_days = current_days.read()
         return stored_days
         
+def get_end_date():
+    date = datetime.now() + timedelta(int(stored_days))
+    end_date = date.strftime('%A %d %B %Y')
+    return end_date
 
     
-def overwrite(filename, data):
-    with open(filename, 'w+') as file:
-        file.writelines(data)    
-        
+ 
 
-    
-
-        
-    
-   
         
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    stored_days = get_stored_days()
-    date = datetime.now() + timedelta(int(stored_days))
-    end_date = date.strftime('%A %d %B %Y')
+    
     if request.method == 'POST':
         write_to_file('data/users.txt', request.form['username'] + '\n')
         return redirect(request.form['username'])
     return render_template('index.html', end_date=end_date)
     
+    
 @app.route('/<username>', methods=['GET', 'POST'])
 def user(username):
     comments = get_all_comments()
+    end_date = get_end_date()
     if request.method == 'POST':
         comment = request.form['comment']
         days = request.form['days']
         add_comments(username, comment, days)
-        
-        overwrite('data/days.txt', days)
+        add_days(days)
+        end_date = get_end_date()
+       
         return redirect(username)
     return render_template("log.html",
-                            username=username, prob_comments=comments)
+                            username=username, prob_comments=comments, end_date=end_date)
     
     
 
